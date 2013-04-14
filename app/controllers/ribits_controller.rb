64cd8ae80c576f2bcab34c9d3ff971f26d77434e
Willy Aguirre <marti1003@hotmail.com>
2013-04-13 20:59:00 -0500

    added the public ribbits page

--- app/controllers/ribits_controller.rb
+++ app/controllers/ribits_controller.rb
@@ -1,4 +1,10 @@
 class RibitsController < ApplicationController
+	
+	def index
+		@ribits = Ribit.all incluse: :user
+		@ribit = Ribit.new
+	end
+
 	def create
 		@ribit = Ribit.new(params[:ribit])
 		@ribit.userid = current_user.id
